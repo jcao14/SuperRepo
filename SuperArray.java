bc7eef6f74eec39acdc1159bd3c34fa5479887c1
jcao14 <jcao@stuy.edu>
2015-12-09 14:35:30 -0500

    Create SuperArray.java

---
+++ SuperArray.java
@@ -0,0 +1,203 @@
+public class SuperArray implements ListInt {
+ 
+    //~~~~~INSTANCE VARS~~~~~
+    //underlying container, or "core" of this data structure:
+    private int[] _data;
+
+    //position of last meaningful value
+    private int _lastPos;
+
+    //size of this instance of SuperArray
+    private int _size;
+
+		
+    //~~~~~METHODS~~~~~
+    //default constructor – initializes 10-item array
+    public SuperArray() { 
+	_data = new int[10];
+	_lastPos = -1; //flag to indicate no lastpos yet
+	_size = 0;	
+    }
+
+		
+    //output array in [a,b,c] format, eg
+    // {1,2,3}.toString() -> "[1,2,3]"
+    public String toString() { 
+	String retStr = "[";
+	for( int i = 0; i < _size; i++ ) {
+	    retStr += _data[i] + ",";
+	}
+	//shave off trailing comma
+	if ( retStr.length() > 1 )
+	    retStr = retStr.substring( 0, retStr.length()-1 );
+	retStr += "]";
+	return retStr;
+    }
+
+		
+    //double capacity of this SuperArray
+    private void expand() {
+	int[] temp = new int[ _data.length * 2 ];
+	for( int i = 0; i < _data.length; i++ )
+	    temp[i] = _data[i];
+	_data = temp;
+    }
+
+		
+    //accessor -- return value at specified index
+    public int get( int index ) { return _data[index]; }
+
+		
+    //mutator -- set value at index to newVal, 
+    //           return old value at index
+    public int set( int index, int newVal) { 
+ 	int temp = _data[index];
+	_data[index] = newVal;
+	return temp;
+    }
+
+
+    // ~~~~~~~~~~~~~~ PHASE II ~~~~~~~~~~~~~~
+    //adds an item after the last item
+    public void add( int newVal ) {
+	if (_size == _data.length) 
+	    expand();
+	    
+	_lastPos += 1;
+	_size += 1;
+	_data[_lastPos] = newVal;
+    }
+
+
+    //inserts an item at index
+    //shifts existing elements to the right
+    public void add( int index, int newVal ) {
+	if (index < _size && index > -1) {
+	    for (int i = _lastPos; i > index - 1; i--) {
+	        _data[i + 1] = _data[i];
+	    }
+	    _data[index] = newVal;
+	    _lastPos++;
+	    _size++;
+	}
+	else
+	    System.out.println("error: index out of range");	
+    }
+
+    //removes the item at index
+    //shifts elements left to fill in newly-empted slot
+    public void remove( int index ) {
+        if (index < _size && index > -1) {
+	    for (int i = index; i < _lastPos; i++) {
+		_data[i] = _data[i+1];
+	    }
+	    _data[_lastPos] = 0;
+	    _lastPos--;
+	    _size--;
+	}
+	else
+	    System.out.println("error: index out of range");	
+    }
+    
+
+    //return number of meaningful items in _data
+    public int size() { return _size; }
+
+    //main method for testing
+    public static void main( String[] args ) {
+	SuperArray curtis = new SuperArray();
+	System.out.println("Printing empty SuperArray curtis...");
+	System.out.println(curtis);
+
+	for( int i = 0; i < curtis._data.length; i++ ) {
+	    curtis.set(i,i*2);
+	    curtis._size++; //necessary bc no add() method yet
+	}
+
+	System.out.println("Printing populated SuperArray curtis...");
+	System.out.println(curtis);
+
+	System.out.println("testing get()...");
+	for( int i = 0; i < curtis._size; i++ ) {
+	    System.out.print( "item at index" + i + ":\t" );
+	    System.out.println( curtis.get(i) );
+	}
+
+	System.out.println("Expanded SuperArray curtis:");
+	curtis.expand();
+	System.out.println(curtis);
+
+	SuperArray mayfield = new SuperArray();
+	System.out.println("Printing empty SuperArray mayfield...");
+	System.out.println(mayfield);
+
+	mayfield.add(5);
+	mayfield.add(4);
+	mayfield.add(3);
+	mayfield.add(2);
+	mayfield.add(1);
+
+	System.out.println("Printing populated SuperArray mayfield...");
+	System.out.println(mayfield);
+
+	mayfield.remove(3);
+	System.out.println("Printing SuperArray mayfield post-remove...");
+	System.out.println(mayfield);
+	mayfield.remove(3);
+	System.out.println("Printing SuperArray mayfield post-remove...");
+	System.out.println(mayfield);
+
+	mayfield.add(3,99);
+	System.out.println("Printing SuperArray mayfield post-insert...");
+	System.out.println(mayfield);
+	mayfield.add(1,88);
+	System.out.println("Printing SuperArray mayfield post-insert...");
+	System.out.println(mayfield);
+	mayfield.add(1,77);
+	System.out.println("Printing SuperArray mayfield post-insert...");
+	System.out.println(mayfield);
+	//*****INSERT ANY ADDITIONAL TEST CALLS HERE*****
+	
+	ListInt leche = new SuperArray();
+	System.out.println("Printing empty LastInt leche...");
+	System.out.println(leche);
+
+	leche.add(5);
+	leche.add(4);
+	leche.add(3);
+	leche.add(2);
+	leche.add(1);
+
+	System.out.println("Printing populated ListInt leche...");
+	System.out.println(leche);
+
+	leche.remove(3);
+	System.out.println("Printing ListInt leche post-remove...");
+	System.out.println(leche);
+	leche.remove(3);
+	System.out.println("Printing ListInt leche post-remove...");
+	System.out.println(leche);
+
+	leche.add(3,99);
+	System.out.println("Printing ListInt leche post-insert...");
+	System.out.println(leche);
+	leche.add(1,88);
+	System.out.println("Printing ListInt leche post-insert...");
+	System.out.println(leche);
+	leche.add(1,77);
+	System.out.println("Printing ListInt leche post-insert...");
+	System.out.println(leche);
+
+	System.out.println("Printing value at index 2");
+	System.out.println(leche.get(2));
+	
+	System.out.println("Setting value at index 2 to 99");
+	leche.set(2,99);
+	System.out.println(leche);
+
+	System.out.println("Printing the size of leche");
+	System.out.println(leche.size());
+
+    }//end main
+		
+}//end clas
